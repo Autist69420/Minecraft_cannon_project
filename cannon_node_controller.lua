e7d43548c39293caf4da0b1fdab8d78e1c627dda
spaceeye <superspaceeye@gmail.com>
2022-11-08 22:53:30 +0300

    test

---
+++ cannon_node_controller.lua
@@ -0,0 +1,31 @@
+---
+--- Created by SpacEeye.
+---
+
+function clear_console()
+    term.clear()
+    term.setCursorPos(1, 1)
+end
+
+-- for communication between cannon parts
+l_modem = peripheral.wrap("back")
+-- for communication between nodes (in the future)
+g_modem = peripheral.wrap("top")
+
+print("Type \"help\" to list all available arguments")
+
+
+i = 0
+
+function redraw()
+    local x, y = term.getCursorPos()
+    term.setCursorPos(1, 2)
+    term.clearLine()
+    i = i + 1
+    term.write("i =", i)
+    term.setCursorPos(x, y)
+end
+
+while true do
+    redraw()
+end
