0f9d5390161e30197b5beb7fe3d719407fd3c81f
spaceeye <superspaceeye@gmail.com>
2022-11-08 23:30:33 +0300

    test

--- cannon_node_controller.lua
+++ cannon_node_controller.lua
@@ -14,10 +14,10 @@ g_modem = peripheral.wrap("top")
 
 clear_console()
 
-print("Type \"help\" to list all available arguments")
-print("Placeholder")
-print("Other text")
-print("")
+--print("Type \"help\" to list all available arguments")
+--print("Placeholder")
+--print("Other text")
+--print("")
 
 i = 0
 
@@ -31,17 +31,49 @@ function redraw()
 end
 
 function async_ui()
+    --while true do
+    --    redraw()
+    --    sleep(0.1)
+    --end
+
     while true do
-        redraw()
         sleep(0.1)
     end
+end
+
+function connect_to_modules()
+    term.setCursorPos(1, 1)
+
+    -- red text on white
+    term.setTextColor(16384)
+    term.setBackgroundColor(1)
+
+    term.write("Trying to connect to the reloading module...")
+    l_modem.open(1)
+    _, msg, __ = rednet.receive("reloader")
+
+    term.setTextColor(32)
+    term.setBackgroundColor(32768)
+    term.write("Connected to the reloading module!", "5", "1")
+
+
+    term.setTextColor(1)
+    term.setBackgroundColor(32768)
 end
 
 function main_loop()
+    local result = connect_to_modules()
+
+    if not result then
+
+        return
+    end
+
     while true do
         mytest = io.read()
+
         sleep(0.1)
     end
 end
 
-parallel.waitForALL(async_ui, main_loop)
+parallel.waitForAll(async_ui, main_loop)
