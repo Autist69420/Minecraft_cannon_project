f91c9f426c7ab6b848e23f8bedcef9e3dab523a8
spaceeye <superspaceeye@gmail.com>
2022-11-09 16:33:55 +0300

    wip

--- startup_loader.lua
+++ startup_loader.lua
@@ -8,10 +8,13 @@ function loader()
         local type = out.readLine()
 
         if type == "cannon_node" then
+            shell.run("rm cannon_node")
+            shell.run("wget https://github.com/SuperSpaceEye/Minecraft_cannon_project/blob/master/cannon_node.lua")
+            shell.run("cannon_node")
 
-        elseif type == "reloader" then
+        elseif type == "reloader_controller" then
 
-        elseif type == "cannon_controler" then
+        elseif type == "cannon_controller" then
 
         elseif type == "main_node" then
 
@@ -19,12 +22,63 @@ function loader()
     end
 
     local function configure_new()
+        temp.clear()
+        temp.setCursorPos(1, 1)
 
+        while true do
+            print("Hello, please choose what part of automatic cannon is this:\n
+            1 - Cannon node\n
+            2 - Cannon controller\n
+            3 - Cannon reloader\n")
+            local answer = io.read()
+
+            if answer == "1" then
+                local file = fs.open("settings", "w")
+                file.write("cannon_node")
+                file.close()
+                os.reboot()
+            elseif answer == "2" then
+                local file = fs.open("settings", "w")
+                file.write("cannon_controller")
+                file.close()
+                os.reboot()
+            elseif answer == "3" then
+                local file = fs.open("settings", "w")
+                file.write("reloader_controller")
+                file.close()
+                os.reboot()
+            else
+                temp.clear()
+                temp.setCursorPos(1, 1)
+
+                print("Not correct option, try again")
+            end
+
+        end
     end
 
-    local function update_version()
-        os.shell("wget ")
+    local function try_update_version()
+        local function compare_files()
+            current_file = fs.open("startup")
+            new_file = fs.open("temp_loader")
+
+            if current_file.readAll() == new_file.readAll() then
+                return true
+            end
+            return false
+        end
+
+        shell.run("wget https://raw.githubusercontent.com/SuperSpaceEye/Minecraft_cannon_project/master/startup_loader.lua temp_loader")
+        if compare_files() then
+            shell.run("rm temp_loader")
+        else
+            shell.run("rm startup")
+            shell.run("rename temp_loader startup")
+            os.reboot()
+        end
     end
+
+    try_update_version()
 
     if fs.exists("settings") then
         load_preset()
